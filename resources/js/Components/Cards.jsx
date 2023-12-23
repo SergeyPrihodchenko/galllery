2382365a3b152d6e28b0026f1fea6e7e9432aaca
Sergo <S.prihodchenko@yandex.ru>
2023-12-23 12:54:52 +0300

    a

--- resources/js/Components/Cards.jsx
+++ resources/js/Components/Cards.jsx
@@ -8,11 +8,12 @@ function ImgOverlayExample({posts}) {
             <Card key={el.id} className="bg-dark text-white cardB">
             <Card.Img src={'./storage/img_drink/'+el.img_drink} alt="Card image" width='860px' height='300px'/>
             <Card.ImgOverlay>
-                <Card.Title>{el.title}</Card.Title>
-                <Card.Text>
-                {el.subtitle}
+                <Card.Title className='text_content'>{el.title}</Card.Title>
+                <Card.Text className='text_content'>
+                {el.subtitle.slice(0, 200) + '...'}
                 </Card.Text>
             </Card.ImgOverlay>
+            <div className='back_fon'></div>
             </Card>
           </Link>
         )})
